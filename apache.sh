65e5695ef3608c30f69db76994497746ef2992f2
diwakarvenkatasai <123432726+diwakarvenkatasai@users.noreply.github.com>
2024-01-11 18:48:18 +0530

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -3,5 +3,5 @@ yum install httpd git -y
 systemctl start httpd
 systemctl status httpd
 cd /var/www/html
-git clone https://github.com/CleverProgrammers/pwj-netflix-clone.git
+git clone https://github.com/diwakarvenkatasai/netflix-clone
 mv pwj-netflix-clone/* .
